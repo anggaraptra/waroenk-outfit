f6e909fc3e646e209ecda7233f606f06c26d3e23
anggaraptra <kadekanggaraptra@gmail.com>
2022-05-07 15:52:11 +0800

    updet

--- admin/edit-barang.php
+++ admin/edit-barang.php
@@ -96,7 +96,7 @@ if (isset($_POST['submitEdit'])) {
                     </li>
                     <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                             <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-                            <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
+                            <div class="d-sm-none d-lg-inline-block">Hi, </div>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
                             <a href="#" class="dropdown-item has-icon text-danger">
